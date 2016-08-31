2d04cad08e482115a1993fd3ec86251e108c3901
zhangsai <1195073382@qq.com>
2016-08-31 15:04:50 +0800

    根据json数据的轮播基础插件完成

--- AiZheAiNa/Scripts/MyLunBo/ZSImgLunBo.js
+++ AiZheAiNa/Scripts/MyLunBo/ZSImgLunBo.js
@@ -32,15 +32,31 @@
         //图片数据加入到img容器中
         $.each(jsonData.data, function (index, item) {
             //加入图片数据
-            if (item.herf == "") {
-                item.herf = "javascript:void(0)";
+            if (item.href == "") {
+                item.href = "javascript:void(0)";
             }
-            $(xdivZSLunBoImg).append($("<a href='" + item.herf + "'><img src='" + item.src + "'></a>"));
+            $(xdivZSLunBoImg).append($("<a href='" + item.href + "'><img src='" + item.src + "'></a>"));
         });
         //轮播标识加入到dom中
         if (op.isShowLunBoBiaoShi) {
             op.container.append($("<div class='divZSLunBoBiaoShiIndivLunBoImg' id='" + divZSLunBoBiaoShiIndivLunBoImg + "'><ul class='ZS-list-inline'></ul></div>"));
             GenJuImgShuShengChengBiaoShi(xdivZSLunBoImg + " a img", xdivZSLunBoBiaoShiIndivLunBoImg + " ul");
+            $(xdivZSLunBoBiaoShiIndivLunBoImg + " ul li").each(function () {
+
+                $(this).mouseout(function () {
+                    $(xdivZSLunBoImg).mouseout();
+                })
+
+                $(this).mousemove(function () {
+                    $(xdivZSLunBoImg).mousemove();
+                    $(xdivZSLunBoBiaoShiIndivLunBoImg + " ul li").each(function () {
+                        $(this).html("○");
+                    });
+                    $(xdivZSLunBoBiaoShiIndivLunBoImg + " ul li").eq($(this).index()).html("●");
+                    //图片出现的方式，需根据轮播方式选择
+                    SwitchImgShowFangShi($(this).index());
+                });
+            });
         }
         //轮播按钮加入到dom中
         if (op.isLunShowBoAnNiu) {
@@ -52,14 +68,9 @@
                     clearInterval(daTuZiDongLunBoID);
                 });
                 //img容器的mouseout需根据轮播方式选择开启自动轮播方式，轮播按钮和轮播标识为mouseout等于img容器的mouseout
-                switch (op.LunBoFangShi.toLowerCase()) {
-                    case "1":
-                        break;
-                    default:
-                        $(xdivZSLunBoImg).mouseout(function () {
-                            DefaultZiDongLunBo(op);
-                        });
-                };
+                $(xdivZSLunBoImg).mouseout(function () {
+                    SwitchZiDongLunBo()
+                });
             };
             //轮播按钮点击及鼠标离开自动轮播执行方式
             $(xdivZSLunBoAnNiuIndivLunBoImg + " div").each(function (index, item) {
@@ -67,12 +78,12 @@
                 if (index == 0) {
                     //后退
                     $i.click(function () {
-                        LunBoAnNiuClick("down", false);
+                        LunBoAnNiuClick("down");
                     });
                 } else {
                     //前进
                     $i.click(function () {
-                        LunBoAnNiuClick("up", true);
+                        LunBoAnNiuClick("up");
                     });
                 };
                 if (op.isOpenZiDongLunBo) {
@@ -88,19 +99,13 @@
         }
         //自动轮播的方式，需根据传入的轮播方式选择
         if (op.isOpenZiDongLunBo) {
-            switch (op.LunBoFangShi.toLowerCase()) {
-                case "1":
-                    break;
-                default:
-                    DefaultZiDongLunBo(op);
-            }
+            SwitchZiDongLunBo();
         };
 
         return daTuZiDongLunBoID;
 
-
         //点击轮播按钮direction=up或down
-        function LunBoAnNiuClick(direction, isZhuCeMouseLunBoBiaoShi) {
+        function LunBoAnNiuClick(direction) {
             //图片index的增减，判断该显示哪张图片
             if (direction == "up") {
                 if ($(xdivZSLunBoImg + " a").length - 1 == ZSlunBoAnNiuClickJiShu) {
@@ -108,7 +113,7 @@
                 } else {
                     ZSlunBoAnNiuClickJiShu++;
                 }
-            } else if (irection == "down") {
+            } else if (direction == "down") {
                 if (ZSlunBoAnNiuClickJiShu == 0) {
                     ZSlunBoAnNiuClickJiShu = $(xdivZSLunBoImg + " a").length - 1;
                 } else {
@@ -117,58 +122,56 @@
             } else {
                 return false;
             }
-            //显示轮播标识
+            //图片出现的方式，需根据轮播方式选择
+            SwitchImgShowFangShi(ZSlunBoAnNiuClickJiShu);
+            //轮播标识的切换
             if (op.isShowLunBoBiaoShi) {
                 $(xdivZSLunBoBiaoShiIndivLunBoImg + " ul li").each(function () {
                     $(this).html("○");
                 });
                 $(xdivZSLunBoBiaoShiIndivLunBoImg + " ul li").eq(ZSlunBoAnNiuClickJiShu).html("●");
             }
+        };
+
+        //--------------------------------------------------在此处增加轮播方式---------------------------------------------------
+
+        //默认自动轮播方式
+        function ZiDongLunBoDefault() {
+            daTuZiDongLunBoID = setInterval(function () {
+                LunBoAnNiuClick("up");
+                ClickLunBoAnNiuShowImgDefault(ZSlunBoAnNiuClickJiShu);
+            }, op.ZiDongLunBoMiao);
+        };
+
+        //默认点击轮播按钮图片怎么出现
+        function ClickLunBoAnNiuShowImgDefault(index) {
+            $(xdivZSLunBoImg + " a").each(function () {
+                $(this).hide();
+            });
+            $(xdivZSLunBoImg + " a").eq(index).show();
+        };
+
+        //根据轮播方式选择自动轮播的方式
+        function SwitchZiDongLunBo() {
             switch (op.LunBoFangShi.toLowerCase()) {
                 case "1":
                     break;
                 default:
-                    DefaultClickLunBoAnNiuShowImg();
+                    ZiDongLunBoDefault();
             }
-            //注册轮播标识的mouse，只需注册一次
-            if (isZhuCeMouseLunBoBiaoShi) {
-                $(xdivZSLunBoBiaoShiIndivLunBoImg + " ul li").each(function () {
-
-                    $(this).mouseout(function () {
-                        $(xdivZSLunBoImg).mouseout();
-                    })
-
-                    $(this).mousemove(function () {
-                        $(xdivZSLunBoImg).mousemove();
-                        $(xdivZSLunBoBiaoShiIndivLunBoImg + " ul li").each(function () {
-                            $(this).html("○");
-                        });
-                        $(xdivZSLunBoBiaoShiIndivLunBoImg + " ul li").eq($(this).index()).html("●");
-                        switch (op.LunBoFangShi.toLowerCase()) {
-                            case "1":
-                                break;
-                            default:
-                                DefaultClickLunBoAnNiuShowImg();
-                        }
-                    });
-                });
-            };
+        };
+        //根据轮播方式选择图片的出现方式
+        function SwitchImgShowFangShi(index) {
+            switch (op.LunBoFangShi.toLowerCase()) {
+                case "1":
+                    break;
+                default:
+                    ClickLunBoAnNiuShowImgDefault(index);
+            }
         };
 
-        //默认轮播方式
-        function DefaultZiDongLunBo(op) {
-            daTuZiDongLunBoID = setInterval(function () {
-                LunBoAnNiuClick("up", false);
-                DefaultClickLunBoAnNiuShowImg();
-            }, op.ZiDongLunBoMiao);
-        };
-        //默认点击轮播按钮图片怎么出现
-        function DefaultClickLunBoAnNiuShowImg() {
-            $(xdivZSLunBoImg + " a").each(function () {
-                $(this).hide();
-            });
-            $(xdivZSLunBoImg + " a").eq(ZSlunBoAnNiuClickJiShu).show();
-        };
+
+        //--------------------------------------------------在此处增加轮播方式---------------------------------------------------
 
         //生成id
         function ShengChengID(str) {
