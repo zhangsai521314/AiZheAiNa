b2394d87258c3efd405192bb343497c9403a90c0
zhangsai <1195073382@qq.com>
2016-09-02 14:50:31 +0800

    增加js生成验证码

---
+++ AiZheAiNa/Scripts/VerificationCode/VerificationCode.js
@@ -0,0 +1,236 @@
+﻿(function () {
+
+    //自动生成时创建验证码内容
+    var randstr = function (length) {
+        var key = {
+            //自动生成时的数据词典
+            str: [
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'o', 'p', 'q', 'r', 's', 't', 'x', 'u', 'v', 'y', 'z', 'w', 'n',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+            ],
+
+            randint: function (n, m) {
+                var c = m - n + 1;
+                var num = Math.random() * c + n;
+                return Math.floor(num);
+            },
+
+            randStr: function () {
+                var _this = this;
+                var leng = _this.str.length - 1;
+                var randkey = _this.randint(0, leng);
+                return _this.str[randkey];
+            },
+
+            create: function (len) {
+                var _this = this;
+                var l = len || 10;
+                var str = '';
+
+                for (var i = 0 ; i < l ; i++) {
+                    str += _this.randStr();
+                }
+
+                return str;
+            }
+        };
+        length = length ? length : 10;
+        return key.create(length);
+    };
+
+    var randint = function (n, m) {
+        var c = m - n + 1;
+        var num = Math.random() * c + n;
+        return Math.floor(num);
+    };
+
+    //初始化整个插件，dom验证码容器，options传入参数
+    var vCode = function (dom, options) {
+        this.codeDoms = [];
+        this.lineDoms = [];
+        this.initOptions(options);//把传入参数赋值给本插件默认的参数，以修改默认参数
+        this.dom = dom;
+        this.init();
+        this.addEvent();
+        this.update();
+        this.mask();
+    };
+
+    //设置验证码容器的属性
+    vCode.prototype.init = function () {
+        this.dom.style.position = "relative";
+        this.dom.style.overflow = "hidden";
+        this.dom.style.cursor = "pointer";
+        this.dom.title = "点击更换验证码";
+        this.dom.style.background = this.options.bgColor;
+        this.w = this.dom.clientWidth;
+        this.h = this.dom.clientHeight;
+        this.uW = this.w / (this.options.isZiDognShengCheng == true ? this.options.len : this.options.cotent.length);
+    };
+
+    //创建点击更换验证码div并设置其属性。整个验证码div最上面的一层
+    vCode.prototype.mask = function () {
+        var dom = document.createElement("div");
+        dom.style.cssText = [
+            "width: 100%",
+            "height: 100%",
+            "left: 0",
+            "top: 0",
+            "position: absolute",
+            "cursor: pointer",
+            "z-index: 9999999"
+        ].join(";");
+        dom.title = "点击更换验证码";
+        this.dom.appendChild(dom);
+    };
+
+    //每当点击验证码的时候都重新执行一遍插件
+    vCode.prototype.addEvent = function () {
+        var _this = this;
+        _this.dom.addEventListener("click", function () {
+            _this.update.call(_this);
+        });
+    };
+
+    //本插件默认参数
+    vCode.prototype.initOptions = function (options) {
+        var f = function () {
+            this.isZiDognShengCheng = true;//是否自己生成验证码
+            this.cotent = "张志霞发财";//不自动生成时的验证码内容
+            this.len = 4;//自动生成时默认生成验证码的数量
+            this.fontSizeMin = 20;
+            this.fontSizeMax = 20;
+            this.colors = [
+                "green",
+                "red",
+                "blue",
+                "#53da33",
+                "#AA0000",
+                "#FFBB00"
+            ];
+            this.bgColor = "#FFF";//默认背景
+            this.fonts = [
+                "Times New Roman",
+                "Georgia",
+                "Serif",
+                "sans-serif",
+                "arial",
+                "tahoma",
+                "Hiragino Sans GB"
+            ];
+            this.lines = 8;
+            this.lineColors = [
+                "#888888",
+                "#FF7744",
+                "#888800",
+                "#008888"
+            ];
+            this.lineHeightMin = 1;
+            this.lineHeightMax = 3;
+            this.lineWidthMin = 1;
+            this.lineWidthMax = 60;
+        };
+
+        this.options = new f();
+
+        if (typeof options === "object") {
+            for (i in options) {
+                this.options[i] = options[i];
+            }
+        }
+    };
+
+    //执行更换验证码
+    vCode.prototype.update = function () {
+        for (var i = 0; i < this.codeDoms.length; i++) {
+            this.dom.removeChild(this.codeDoms[i]);
+        }
+        for (var i = 0; i < this.lineDoms.length; i++) {
+            this.dom.removeChild(this.lineDoms[i]);
+        }
+        this.createCode();
+        this.draw();
+    };
+
+    //执行创建验证码内容
+    vCode.prototype.createCode = function () {
+        if (this.options.isZiDognShengCheng) {
+            this.code = randstr(this.options.len);
+        } else {
+            this.code = this.options.cotent;
+        }
+    };
+
+    //执行验证
+    vCode.prototype.verify = function (code) {
+        return this.code === code;
+    };
+
+    //执行绘制
+    vCode.prototype.draw = function () {
+        this.codeDoms = [];
+        for (var i = 0; i < this.code.length; i++) {
+            this.codeDoms.push(this.drawCode(this.code[i], i));
+        }
+        this.drawLines();
+    };
+
+    //绘制内容的span属性设置
+    vCode.prototype.drawCode = function (code, index) {
+        var dom = document.createElement("span");
+        dom.style.cssText = [
+            "font-size:20px",
+            "color:" + this.options.colors[randint(0, this.options.colors.length - 1)],
+            "position: absolute",
+            "left:" + randint(this.uW * index, this.uW * index + this.uW - 10) + "px",
+            "top:" + randint(0, this.h - 30) + "px",
+            "transform:rotate(" + randint(-30, 30) + "deg)",
+            "-ms-transform:rotate(" + randint(-30, 30) + "deg)",
+            "-moz-transform:rotate(" + randint(-30, 30) + "deg)",
+            "-webkit-transform:rotate(" + randint(-30, 30) + "deg)",
+            "-o-transform:rotate(" + randint(-30, 30) + "deg)",
+            "font-family:" + this.options.fonts[randint(0, this.options.fonts.length - 1)],
+            "font-weight:" + randint(400, 900)
+        ].join(";");
+
+
+
+
+        dom.innerHTML = code;
+        this.dom.appendChild(dom);
+
+        return dom;
+    };
+
+    //绘制干扰线的sapn属性设置
+    vCode.prototype.drawLines = function () {
+        this.lineDoms = [];
+        for (var i = 0; i < this.options.lines; i++) {
+            var dom = document.createElement("div");
+            dom.style.cssText = [
+                "position: absolute",
+                "opacity: " + randint(3, 8) / 10,
+                "width:" + randint(this.options.lineWidthMin, this.options.lineWidthMax) + "px",
+                "height:" + randint(this.options.lineHeightMin, this.options.lineHeightMax) + "px",
+                "background: " + this.options.lineColors[randint(0, this.options.lineColors.length - 1)],
+                "left:" + randint(0, this.w - 20) + "px",
+                "top:" + randint(0, this.h) + "px",
+                "transform:rotate(" + randint(-30, 30) + "deg)",
+                "-ms-transform:rotate(" + randint(-30, 30) + "deg)",
+                "-moz-transform:rotate(" + randint(-30, 30) + "deg)",
+                "-webkit-transform:rotate(" + randint(-30, 30) + "deg)",
+                "-o-transform:rotate(" + randint(-30, 30) + "deg)",
+                "font-family:" + this.options.fonts[randint(0, this.options.fonts.length - 1)],
+                "font-weight:" + randint(400, 900)
+            ].join(";");
+            this.dom.appendChild(dom);
+
+            this.lineDoms.push(dom);
+        }
+    };
+
+    //程序入口
+    this.vCode = vCode;
+
+}).call(this);
