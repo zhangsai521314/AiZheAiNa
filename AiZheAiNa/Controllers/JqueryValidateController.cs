9a1930bd4aefeaca58561626fc07babf7255c099
zhangsai <1195073382@qq.com>
2016-09-06 17:34:20 +0800

    添加/JqueryValidate示例模板

---
+++ AiZheAiNa/Controllers/JqueryValidateController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AiZheAiNa.Controllers
+{
+    public class JqueryValidateController : Controller
+    {
+        // GET: JqueryValidate
+        public ActionResult Index()
+        {
+    
+            return View();
+        }
+    }
+}
