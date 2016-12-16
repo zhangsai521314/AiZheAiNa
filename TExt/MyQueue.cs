d9d5d1d071a9e0dffc1a13d2c9ce84f19c92cfd3
zhangsai <1195073382@qq.com>
2016-12-16 16:41:36 +0800

    s

---
+++ TExt/MyQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuiLiYanCeShi
+{
+    public class MyQueue
+    {
+        //原理：利用生产者消费者模式进行入列出列操作  
+
+        public readonly static MyQueue Instance = new MyQueue();
+        private MyQueue()
+        {
+        }
+
+        public Queue<QueueInfo> ListQueue = new Queue<QueueInfo>();
+
+        public void AddQueue(string medias, string proids, string host, string userid, string feedid) //入列  
+        {
+            QueueInfo queueinfo = new QueueInfo();
+
+            queueinfo.medias = medias;
+            queueinfo.proids = proids;
+            queueinfo.host = host;
+            queueinfo.userid = userid;
+            queueinfo.feedid = feedid;
+            ListQueue.Enqueue(queueinfo);
+        }
+
+
+        public class QueueInfo
+        {
+            public string medias { get; set; }
+            public string proids { get; set; }
+            public string host { get; set; }
+            public string userid { get; set; }
+            public string feedid { get; set; }
+        }
+    }
+
+}
+
---
+++ TExt/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static DuiLiYanCeShi.MyQueue;
+
+namespace DuiLiYanCeShi
+{
+    class Program
+    {
+        private static object lockObject = new Object();
+        static void Main(string[] args)
+        {
+            Thread threads = new Thread(s);
+            threads.IsBackground = true;
+            threads.Start();
+
+            Thread threadg = new Thread(g);
+            threadg.IsBackground = true;
+            threadg.Start();
+            Console.ReadKey();
+        }
+        public static void s()
+        {
+            int i = 1;
+            while (true)
+            {
+                Console.WriteLine("加入了:" + i);
+                MyQueue.Instance.AddQueue(i.ToString(), (i + 1).ToString(), (i + 2).ToString(), (i + 3).ToString(), (i + 4).ToString());
+                Console.WriteLine("加入结束了:" + i);
+                i++;
+            }
+        }
+
+        public static void g()
+        {
+            while (true)
+            {
+                Queue<QueueInfo> q = MyQueue.Instance.ListQueue;
+                if (q.Count > 0)
+                {
+                    lock (lockObject)
+                    {
+                        Console.WriteLine("队列数量：" + q.Count);
+                        QueueInfo queueinfo = q.Dequeue();
+                        Console.WriteLine("队列数量减少后：" + q.Count);
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
