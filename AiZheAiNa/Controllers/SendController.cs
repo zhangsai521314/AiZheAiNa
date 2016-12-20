13a0ace5e936cabd422d8c56bcb05f1c47ed62b0
zhangsai <1195073382@qq.com>
2016-12-20 18:20:17 +0800

    email

---
+++ AiZheAiNa/Controllers/SendController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AiZheAiNa.Controllers
+{
+    public class SendController : Controller
+    {
+        // GET: Send
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void SendEmail(string email)
+        {
+
+        }
+    }
+}
